c85cfbaf3ce792c54ee054f0f32b48974184940d
i0gan <42000708+i0gan@users.noreply.github.com>
2022-03-07 14:13:28 +0800

    Add ProcessBar
    增加进度条

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 加载游戏菜单
+        Invoke("LoadGameMenuScene", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadGameMenuScene()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 2);
+        AsyncOperation op = SceneManager.LoadSceneAsync("Scenes/GameMenu");
+        op.allowSceneActivation = true;
+    }
+}
