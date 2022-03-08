7fa0f26b20a7a52db4d1ca2cd6a6ef418f3b1ab9
tomoka-itoh <tomokaitoh@163.com>
2022-03-08 23:26:15 +0800

    给被选中物体加入框选效果

--- Assets/Player/Scripts/RayGunController.cs
+++ Assets/Player/Scripts/RayGunController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RayGunController : MonoBehaviour{
-    LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
+    public GameObject targetObj;
     void Start(){
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.startWidth = 0.08f;
@@ -17,28 +18,34 @@ public class RayGunController : MonoBehaviour{
     }
 
     //绘制射线
-    void DrawRay()
-    {
+    void DrawRay(){
         //获取鼠标位置
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = transform.position.z;
+        
+        //射线目标位置
+        Vector3 targetPos;
 
         //创建射线并获取打到的物体
         Ray2D ray = new Ray2D(transform.position, mousePos - transform.position);
         RaycastHit2D info = Physics2D.Raycast(ray.origin, ray.direction, 100, 1 << 7, -10);
         if (info.collider != null){
             GameObject obj = info.collider.gameObject;
-            //Debug.Log(obj.name);
+            targetPos = new Vector3(info.point.x, info.point.y, transform.position.z);
+            //绘制框架
+            targetObj.transform.position = obj.transform.position;
         }
+        else targetPos = mousePos;
 
 
         //绘制射线(绘制在最下层,所以z轴坐标加1)
         lineRenderer.SetPosition(0, transform.position + new Vector3(0,0,1));
-        lineRenderer.SetPosition(1, mousePos + new Vector3(0, 0, 1));
+        lineRenderer.SetPosition(1, targetPos + new Vector3(0, 0, 1));
 
         if (Input.GetMouseButtonDown(0)){
             Debug.Log(mousePos);
         }
     }
+
 
 }
