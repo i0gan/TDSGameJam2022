12d9e17317c67e3fbed7e87062fdbbf263cd5250
tomoka-itoh <tomokaitoh@163.com>
2022-03-08 19:32:07 +0800

    角色,运动脚本,测试场景

---
+++ Assets/Player/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour{
+    Rigidbody2D rb;
+
+    public float speed = 14;
+    public float jumpSpeed = 7;
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //获取角色水平方向移动输入
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0){
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+            //通过x轴缩放改变角色朝向
+            float x_scale = (horizontal > 0 ? -1:1) * Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector3(x_scale, transform.localScale.y, transform.localScale.z);
+        }
+
+        //角色跳跃
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0){
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+}
