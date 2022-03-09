b8b91b468f3a4cc3a3b57a0df52874b86e748ad4
tomoka-itoh <tomokaitoh@163.com>
2022-03-09 19:42:17 +0800

    test

--- Assets/Scripts/TTT.cs
+++ Assets/Scripts/TTT.cs
@@ -7,12 +7,12 @@ public class TTT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //aasdasddd
+        //AAAAAAAAAAAAAAAAAAAAAAAAAAA
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //AAAAAAAAAAAAAAAAAAAAAAAAAAA
     }
 }
