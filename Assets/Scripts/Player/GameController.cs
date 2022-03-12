fcba980345921e2d91624ca8fea7d4e80ae1cb78
tomoka-itoh <tomokaitoh@163.com>
2022-03-12 14:51:24 +0800

    加入字幕效果

--- Assets/Scripts/Player/GameController.cs
+++ Assets/Scripts/Player/GameController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController:MonoBehaviour{
     public int score = 0;
     public float time = 0;
     public LevelUI ui;
-
+    public Text textUI;
+    private GameObject textObj;
     
     public void Init(){
+        textObj = (GameObject)Instantiate(Resources.Load("UI/Text"), new Vector3(1000,120,0), transform.rotation);
+        textObj.transform.parent = GameObject.Find("Canvas").transform;
         ui = GameObject.Find("Canvas/GameLevelUI").GetComponent<LevelUI>();
+        textUI = textObj.GetComponent<Text>();
+        textUI.enabled = false;
+
         score = 0;
         time = 0;
     }
@@ -34,5 +41,13 @@ public class GameController:MonoBehaviour{
     public void victory(){
         ui.ShowGamePass(score, (int)time);
         AudioManager.GetInstance.PlayAudio(AudioManager.AudioType.Victory);
+    }
+    public void setSubtitle(string text){
+        textUI.enabled = true;
+        textUI.text = text;
+    }
+    public void HideSubtitle()
+    {
+        textUI.enabled = false;
     }
 }
